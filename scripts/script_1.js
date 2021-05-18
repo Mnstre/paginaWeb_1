6bf76dfceb6849074ddce39af932bb23abc3a624
Miguel <gmiguel.503@gmail.com>
2021-05-18 13:39:18 -0500

    1q

--- scripts/script_1.js
+++ scripts/script_1.js
@@ -1,4 +1,5 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+/*
 window.onscroll = function() {
     scrollFunction()
 };
@@ -12,9 +13,9 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
-
+*/
 // Add slideDown animation to Bootstrap dropdown when expanding.
-var drop_1 = document.getElementById('drop1');
+//var drop_1 = document.getElementById('drop1');
 
 /*
 $('.dropdown').on('show.bs.dropdown', function() {
@@ -25,3 +26,31 @@ $('.dropdown').on('show.bs.dropdown', function() {
   $('.dropdown').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });*/
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("DOMContentLoaded", function() {
+  el_autohide = document.querySelector('.autohide');
+  
+  // add padding-top to bady (if necessary)
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+
+  if(el_autohide){
+    var last_scroll_top = 80;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else if (scroll_top > 280) {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    }); 
+    // window.addEventListener
+  }
+  // if
+
+});
